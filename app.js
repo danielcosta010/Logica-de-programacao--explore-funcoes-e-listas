d205ec26dcce5fe7efaff531bed2de9477604790
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-06-16 03:26:04 -0300

    aula3

--- app.js
+++ app.js
@@ -1,21 +1,65 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag)
   campo.innerHTML = texto
+};
+
+
+function exibirMensagemInicial() {
+  exibirTextoNaTela('h1', 'Jogo do número secreto');
+  exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
+exibirMensagemInicial()
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+function seInputVazio() {
+  const input = document.querySelector('input');
+  const botao = document.getElementById('chutar');
 
-let numeroUsuario = document.querySelector('.container__input')
+  input.addEventListener('input', function () {
+    if (input.value.trim() === '') {
+      botao.setAttribute('disabled', true)
+    } else {
+      botao.removeAttribute('disabled')
+    }
+  });
+}
 
 function verificarChute() {
   let chute = document.querySelector('input').value
-  console.log(chute == numeroSecreto);
+  let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+  let mensagemTentativas = `O numero secreto era ${chute}, e voçê acertou com ${tentativas} ${palavraTentativa}`;
+  if (chute == numeroSecreto) {
+    exibirTextoNaTela('h1', 'Acertou!!!')
+    exibirTextoNaTela('p', mensagemTentativas)
+    document.getElementById('reiniciar').removeAttribute('disabled')
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela('p', 'O número secreto é menor que ' + chute)
+    } else {
+      exibirTextoNaTela('p', 'O número secreto é maior que ' + chute)
+    }
+  }
+  tentativas++;
+  limpaCampo();
+
 }
 
 function gerarNumeroAleatorio() {
   return parseInt(Math.random() * 10 + 1)
 };
 
+function limpaCampo() {
+  chute = document.querySelector('input').value = ''
+  seInputVazio()
+};
+
+function reiniciarJogo() {
+  numeroSecreto = gerarNumeroAleatorio()
+  exibirMensagemInicial()
+  limpaCampo()
+  tentativas = 1
+  document.getElementById('reiniciar').setAttribute('disabled', true)
+
+}
